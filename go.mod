7c99513c33451c35c84da00f54ece2509c29900b
Dan Manners <daniel.a.manners@gmail.com>
2022-02-26 17:15:56 -0500

    Functional, but shitty?

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.17
 require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/render v1.0.1
+	github.com/google/uuid v1.3.0
 )
--- testapi.go
+++ testapi.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/google/uuid"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -19,14 +22,52 @@ func main() {
 	r.Use(middleware.URLFormat)
 	r.Use(render.SetContentType(render.ContentTypeJSON))
 
+	// Creating the struct
+	secrets := make(map[string]string)
+
 	// Offer up a pong response to check if things are responsive
 	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
+		fmt.Fprintf(w, "pong")
 	})
 
 	//
 	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
 		panic("test")
+	})
+
+	// Create a secret from a data post
+	r.Post("/secret", func(w http.ResponseWriter, r *http.Request) {
+		s := uuid.New().String()
+
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return
+		}
+
+		fmt.Printf("Body: %s\nBodyLen: %d\n", body, len(body))
+
+		if string(body) != "" {
+			secrets[s] = string(body)
+			fmt.Printf("Wrote '%s' to '%s'.\n", string(body), s)
+			fmt.Fprintf(w, "%s\n", s)
+		} else {
+			fmt.Fprintf(w, "No data was sent\n")
+		}
+
+	})
+
+	//Get Secret
+	r.Get("/{secret}", func(w http.ResponseWriter, r *http.Request) {
+		data := chi.URLParam(r, "secret")
+		fmt.Printf("'%s' fetch was attempted.\n", data)
+		s := secrets[data]
+		if s != "" {
+			w.Write([]byte(fmt.Sprintf("%s\n", s)))
+		} else {
+			w.Write([]byte(fmt.Sprintf("'%s' no longer or has never existed.\n", data)))
+		}
+		// Kill the secret
+		delete(secrets, data)
 	})
 
 	listeningPort := functions.GetEnv("tapi_port", "3000")
